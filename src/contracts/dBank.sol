4c2eb3e181d9c12bf77c25f0ac1c5df1f35be7e3
David <sharp2448@seznam.cz>
2021-05-31 17:14:04 +0200

    token contract and dbank contract - alpha version(v0.1)

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -14,6 +14,7 @@ contract dBank {
 
     //events
     event Deposit(address indexed user, uint256 etherAmount, uint256 timeStart);
+    event Withdraw(address indexed user, uint256 etherAmount, uint256 depositTime, uint256 interest);
 
     //pass as constructor argument deployed Token contract
     constructor(Token _token) public {
@@ -23,10 +24,7 @@ contract dBank {
 
     function deposit() public payable {
         //check if msg.sender didn't already deposited funds
-        require(
-            isDeposited[msg.sender] == false,
-            "Error, deposit already active"
-        );
+        require(isDeposited[msg.sender] == false, "Error, deposit already active");
         //check if msg.value is >= than 0.01 ETH=(1e16 or 10**16)
         require(msg.value >= 10**16, "Error, deposti must be >= 0.01 ETH");
 
@@ -43,14 +41,32 @@ contract dBank {
 
     function withdraw() public {
         //check if msg.sender deposit status is true
-        //assign msg.sender ether deposit balance to variable for event
+        require(isDeposited[msg.sender] == true, "Error, no previous deposit");
+        //msg.sender ether deposit balance to variable for event
+        uint256 userBalance = etherBalanceOf[msg.sender];
+
         //check user's hodl time
-        //calc interest per second
-        //calc accrued interest
+        uint256 depositTime = block.timestamp - depositStart[msg.sender];
+        //calc interest per second and calc accrued interest
+        //31668017 - interest(10% APY) per second for min. deposit amount (0.01 ETH), cuz:
+        //1e15(10% of 0.01 ETH) / 31577600 (seconds in 365.25 days)
+
+        //(etherBalanceOf[msg.sender] / 1e16) - calc. how much higher interest will be (based on deposit), e.g.:
+        //for min. deposit (0.01 ETH), (etherBalanceOf[msg.sender] / 1e16) = 1 (the same, 31668017/s)
+        //for deposit 0.02 ETH, (etherBalanceOf[msg.sender] / 1e16) = 2 (doubled, (2*31668017)/s)
+        uint256 interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+        uint256 interest = interestPerSecond * depositTime;
+
         //send eth to user
+        msg.sender.transfer(userBalance);
         //send interest in tokens to user
+        token.mint(msg.sender, interest);
         //reset depositer data
+        depositStart[msg.sender] = 0;
+        etherBalanceOf[msg.sender] = 0;
+        isDeposited[msg.sender] = false;
         //emit event
+        emit Withdraw(msg.sender, userBalance, depositTime, interest);
     }
 
     function borrow() public payable {
